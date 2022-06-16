992edb57fbed29acde96cfa56cf95c36d771e56b
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2022-06-15 22:23:21 -0500

    Code refactoring

---
+++ .github/scripts/build-js.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const uglifyjs = require('uglify-js');
+const helper = require('./helper');
+
+const jsFileList = glob.sync(path.join(helper.buildPath, 'styles/**/theme/js/*.js'))
+	.concat(glob.sync(path.join(helper.buildPath, 'adm/style/js/*.js')));
+
+jsFileList.forEach((j) => {
+	if (j.endsWith('.min.js')) {
+		return;
+	}
+
+	const minFileName = j.replace('.js', '.min.js');
+	const isMinified = fs.existsSync(minFileName);
+
+	if (isMinified){
+		return;
+	}
+
+	const js = fs.readFileSync(j).toString();
+	const result = uglifyjs.minify(js, {
+		toplevel: true,
+		output: {
+			quote_style: 1,
+			shebang: false
+		},
+		mangle: {
+			toplevel: true
+		}
+	});
+
+	fs.writeFileSync(minFileName, result.code, {mode: 0o644});
+});
